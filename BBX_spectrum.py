ab64a787ccd310943ecc6b6581e99e336040d03a
Oskar <of564@york.ac.uk>
2022-12-16 17:04:32 +0000

    User input work

--- BBX_spectrum.py
+++ BBX_spectrum.py
@@ -7,51 +7,6 @@ import numpy as np
 import re
 import pprint
 
-# function to convert to subscript
-def get_sub(x):
-    normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-=()"
-    sub_s = "ₐ₈CDₑբGₕᵢⱼₖₗₘₙₒₚQᵣₛₜᵤᵥwₓᵧZₐ♭꜀ᑯₑբ₉ₕᵢⱼₖₗₘₙₒₚ૧ᵣₛₜᵤᵥwₓᵧ₂₀₁₂₃₄₅₆₇₈₉₊₋₌₍₎"
-    res = x.maketrans(''.join(normal), ''.join(sub_s))
-    return x.translate(res)
-
-def graph_char(): 
-#Graph characterisation - formats plot
-    plt.grid(True)
-    title = str(file)
-    plt.xlabel("Tip Bias / V", fontsize = 10)
-    plt.ylabel("I{}/I{} / Hz/nA ".format(get_sub('E'),get_sub('a')), fontsize = 10)
-    #plt.xticks(np.arange(min(df['Kinetic Energy (eV)']), max(df['Kinetic Energy (eV)'])+10, 10.0), fontsize=8)
-    plt.yticks(fontsize=8)
-    plt.title(title, fontsize = 18)
-    plt.legend(fontsize = 10)
-    
-
-#Select the desired files 
-path = "*00[1-9].dat"
-path2 = "*01[0-2].dat"
-items = glob.glob(path)
-items2 = glob.glob(path2)
-project_files = (items + items2) #Added to a list containing all required files for analysis
-
-
-#If Backward scan performed. Need to sort out Index so all data reads the same in script
-    
-#os.path.split
-#pathname = "C:/Users/oskar/Desktop/My_Scripts/2022-10-12/BBX_HOPG_001.dat"
-
-#(dirname, filename) = os.path.split(pathname)
-#(shortname, extension) = os.path.splitext(filename)
-
-# =============================================================================
-# metadata = os.stat('BBX_spectrum.py')
-# print(time.localtime(metadata.st_mtime))
-# =============================================================================
-
-#sub_path = '*\\*.dat'
-
-#Load each file at a time
-my_list=[]
-#title_list = []
 
 #Define variables
 TC = "Tip Current (A)"
@@ -60,12 +15,43 @@ BB = "BB Back Bias (V)"
 KE = "Kinetic Energy (eV)"
 Hz = "BBX (Hz)"
 Nm = "Normalised data"
+Av = "Average"
 
+# function to convert to subscript
+def get_sub(x):
+    normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-=()"
+    sub_s = "ₐ₈CDₑբGₕᵢⱼₖₗₘₙₒₚQᵣₛₜᵤᵥwₓᵧZₐ♭꜀ᑯₑբ₉ₕᵢⱼₖₗₘₙₒₚ૧ᵣₛₜᵤᵥwₓᵧ₂₀₁₂₃₄₅₆₇₈₉₊₋₌₍₎"
+    res = x.maketrans(''.join(normal), ''.join(sub_s))
+    return x.translate(res)
+
+    plt.legend(fontsize = 10)
+
+#Select the desired files 
+path = "C:/Users/oskar/Desktop/My_Scripts/Si(111)/2022_08_23/"
+file_name = "BBX_Spectroscopy_00[1-3].dat"
+#path2 = "*04[0-4].dat"
+items = glob.glob(path + file_name)
+#items2 = glob.glob(path2)
+project_files = items #+ items2 #Added to a list containing all required files for analysis
+
+#User defined path - Using menus, select Material and then date. 
+    
+print(path, "\n\n")
+my_list=[]
+
+
+def graph_char(): 
+#Graph characterisation - formats plot
+    plt.grid(True)
+    title = str(file)
+    plt.xlabel("Kinetic Energy / eV", fontsize = 10)
+    plt.ylabel("I{}/I{} / Hz/nA ".format(get_sub('E'),get_sub('a')), fontsize = 10)
+    plt.yticks(fontsize=8)
+    plt.title(title, fontsize = 8)
 
 
 for file in project_files:
     
-    #print("Loading file\n"+ file)  #loading message to show data has been taken in and processed successfully
     #Dataframe for graphical use
     line_counter = 0
     r = re.compile("DATA")
@@ -74,7 +60,6 @@ for file in project_files:
             line_counter += 1
             if r.search(l):
                 break
-              
         
     #print("Count =", line_counter)   
     df = pd.read_csv(file, delimiter='\t',skiprows = line_counter, usecols=(TC, BB, Hz))
@@ -92,41 +77,107 @@ for file in project_files:
     if min_index == 0:
         my_list.append(df)
         min_index = df[KE].idxmin()
-        print("Index=", min_index)
+        #print("Index=", min_index)
     else:
         df = df.loc[::-1].reset_index(drop=True)
         my_list.append(df)
         min_index = df[KE].idxmin()
-        print("New Index = ", min_index)
+        #print("New Index = ", min_index)
         
-    
+# =============================================================================
+#     single_graphs=input("Press y to show plots of all the data:")
+#     if single_graphs == "y":
+# =============================================================================
     df.plot.line(x = KE, y = Nm)
     graph_char()
-
+    plt.show()
+        
 length = len(my_list)
+
 average = pd.concat(my_list, axis=1, join="inner")   #Dataframe of all Normalised value
 average = average.T.drop_duplicates().T          #Use DataFrame.drop_duplicates() to Remove Duplicate Columns
 average.drop(([Hz, TCN]), inplace=True, axis=1)
+average[Av] = average[Nm].mean(axis=1)
+    #Loss graph of average
+loss_df = average[[KE, Av]].copy()
+Index_elastic = loss_df[Av].idxmax()
+Elastic_energy = loss_df.loc[Index_elastic, KE]
+loss_df[KE] = (loss_df[KE] - Elastic_energy)*-1
 
 #my_list[0].plot.line(x=KE, y=Nm)
 
 
-def Group():
+def group():
     fig, ax = plt.subplots()
     for i in range(length):
         my_list[i].plot.line(ax=ax, x = KE, y = Nm, label = project_files[i])
         graph_char()
         plt.title("Graphs plotted together")
-        #fig.set_size_inches(14,8)
-
+        fig.set_size_inches(14,8)
+    plt.show()  
+    
 def Average():
-    average["Average"] = average[Nm].mean(axis=1)
-    average.plot.line(x=KE, y="Average")
+    average.plot.line(x=KE, y=Av)
     graph_char()
     title_avg =  "Graph averaged from {} data files".format(length)
     plt.title(title_avg)
+    plt.show()
+  
 
-#Plot Average data together
+def loss():
+    loss_df.plot.line(x = KE, y =Av, color = 'g')
+    graph_char()
+    plt.title("Loss features as a function of Kinetic energy")
+
+    print("Here is the full graph, do you want to set the x limits of the graph?")
+    plt.show()
+    while True:
+        Prompt = input("Answer y or n\n")
+        if Prompt in ['y', 'yes']:
+            x_lower = int(input("Enter the lower limit:"))
+            x_higher = int(input("Enter the higher limit:"))
+            
+            loss_df.plot.line(x = KE, y =Av, color = 'g')
+            plt.xlim(x_lower, x_higher)
+            graph_char()
+            plt.title("Loss features as a function of Kinetic energy")
+            plt.show()
+            break
+        elif Prompt in ['n', 'no']:
+            print("You are happy with the graph, thank you")
+            break
+
+def main():
+    print("Files loaded and graphs ready to be plotted \n There are three options:")
+    print(" 1 to plot all the data on one group\n 2 to plot average of the data\n 3 to plot average as a loss function")
+    User_graph = int(input("Enter number between 1-3:"))
+    if User_graph == 1:
+        group()
+    elif User_graph == 2:
+        Average()
+    elif User_graph == 3:
+        loss()
+    else:
+        print("You entered a wrong number")
+    restart=input("Do you want to generate more plots\t\t\t Press y to continue?")
+    if restart == "y":
+        print("\n\n\n")
+        main()
+    else:
+        print("\nGoodbye")
+    
+main()
+
+
+#Plot loss features and then get Guassian peaks for the data
+
+
+
+
+
+
+
+
 
 
     
